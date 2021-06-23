0e28a3605e041947792bccfacb82c8e7f158fa9f
arpita739 <arpitahalder739@gmail.com>
2021-06-23 20:07:29 +0530

    added multiple inheritance:constructor

--- InheritanceCons.cpp
+++ InheritanceCons.cpp
@@ -1,5 +1,8 @@
 #include<iostream>
+#include <string>
 using namespace std;
+
+/* Single Inheritance
 class Father{
 protected:
     int height;
@@ -27,6 +30,52 @@ class Child : Father //inheritance
 int main()
 {
     Child c(24);
+    c.display();
+    return 0;
+}
+*/
+
+// Multiple Inheritance
+class Father{
+protected:
+    int height;
+public:
+    Father()
+    {
+        cout<<"constructor of father class is called"<<endl;
+        
+    }
+
+};
+class Mother{
+protected:
+    string skincolor;
+public:
+    Mother()
+    {
+        cout<<"constructor of mother class is called"<<endl;
+        
+    }
+
+};
+class Child : public Father,public Mother //inheritance
+{
+    public:
+    Child(int h,string color ):Father(),Mother()  // Inheriting the constructor of Father class in Child constructor of child class
+    {
+        cout<<"child class constructor"<<endl;
+        height=h;
+        skincolor=color;
+    }   
+    void display()
+{
+    cout<<"height is "<<height<<" and color is "<<skincolor<<endl;
+} 
+};
+
+int main()
+{
+    Child c(24,"white");
     c.display();
     return 0;
 }
