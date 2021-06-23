9f9867e7a1dfe5cfe8c343e4980249d7f7aa70a0
arpita739 <arpitahalder739@gmail.com>
2021-06-23 20:49:29 +0530

    access the overwritten base class using scope resolution

--- OverrideMethod.cpp
+++ OverrideMethod.cpp
@@ -1,7 +1,7 @@
 #include<iostream>
 
 using namespace std;
-
+/*
 class Person{
     public:
     void introduce()
@@ -22,5 +22,32 @@ int main()
 {
     Student s;
     s.introduce();
+    return 0;
+}
+*/
+
+// How to access the base class overridden method in dertived class
+class Person{
+    public:
+    void introduce()
+    {
+        cout<<"I am person"<<endl;
+    }
+
+};
+class Student : public Person{
+    public:
+    void introduce() //Override the base class
+    {
+        cout<<"I am person and Student"<<endl;
+        Person ::introduce(); //base class method accessing
+    }
+
+};
+int main()
+{
+    Student s;
+    s.introduce();
+    s.Person :: introduce(); //access the base class overridden method in dertived class using scope resolution
     return 0;
 }
